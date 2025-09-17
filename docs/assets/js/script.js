675b7bc10e4e63c02d6eea902008ec56c3445e62
sixthmonth06 <draljk@gmail.com>
2025-09-17 21:03:19 +0800

    update website

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -97,27 +97,24 @@ for (let i = 0; i < formInputs.length; i++) {
 
 
 
-// page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
+navigationLinks.forEach((link, linkIndex) => {
+  link.addEventListener("click", () => {
+    pages.forEach((page, pageIndex) => {
+      if (link.innerHTML.toLowerCase() === page.dataset.page) {
+        page.classList.add("active");
+        navigationLinks.forEach(l => l.classList.remove("active")); // remove active from all links
+        link.classList.add("active"); // add active only to clicked link
         window.scrollTo(0, 0);
       } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+        page.classList.remove("active");
       }
-    }
-
+    });
   });
-}
+});
+
 
 // project item navigation
 const projectItems = document.querySelectorAll(".project-item");
